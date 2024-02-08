4892adc187c936efa49a2bcfa346943c1d25e288
Xanthian <xanneh@hotmail.com>
2024-02-08 21:48:31 +0000

    1.20.4 Fabric 1.5 Adds support for BiomeMakeover, Ecologics & Promenade

--- src/main/java/net/xanthian/variantchests/datagen/RecipeGenerator.java
+++ src/main/java/net/xanthian/variantchests/datagen/RecipeGenerator.java
@@ -28,7 +28,7 @@ public class RecipeGenerator extends FabricRecipeProvider {
         super(output);
     }
 
-    public static void offerChestRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible chest, ItemConvertible plank) {
+    public static void offerChestRecipe(RecipeExporter exporter, ItemConvertible chest, ItemConvertible plank) {
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, chest, 1)
                 .input('P', plank)
                 .pattern("PPP").pattern("P P").pattern("PPP")
@@ -36,7 +36,7 @@ public class RecipeGenerator extends FabricRecipeProvider {
                 .offerTo(exporter);
     }
 
-    public static void offerCustomChestBoatRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible chestboat, ItemConvertible boat, TagKey<Item> chest) {
+    public static void offerCustomChestBoatRecipe(RecipeExporter exporter, ItemConvertible chestboat, ItemConvertible boat, TagKey<Item> chest) {
         ShapelessRecipeJsonBuilder.create(RecipeCategory.TRANSPORTATION, chestboat)
                 .input(chest).input(boat).group("chest_boat")
                 .criterion("has_boat", RecipeProvider.conditionsFromTag(ItemTags.BOATS))
@@ -44,7 +44,7 @@ public class RecipeGenerator extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(Consumer<RecipeJsonProvider> exporter) {
+    public void generate(RecipeExporter exporter) {
 
         offerChestRecipe(exporter, Vanilla.ACACIA_CHEST, Items.ACACIA_PLANKS);
         offerChestRecipe(exporter, Vanilla.BAMBOO_CHEST, Items.BAMBOO_PLANKS);
@@ -110,11 +110,11 @@ public class RecipeGenerator extends FabricRecipeProvider {
                 .offerTo(exporter, new Identifier("variantchests", "chest"));
     }
 
-    public void registerChestRecipe(Consumer<RecipeJsonProvider> exporter, Map<Identifier, Block> chests, String modId) {
+    public void registerChestRecipe(RecipeExporter exporter, Map<Identifier, Block> chests, String modId) {
         registerChestRecipe(exporter, chests, modId, "_planks");
     }
 
-    public void registerChestRecipe(Consumer<RecipeJsonProvider> exporter, Map<Identifier, Block> chests, String modId, String plankSuffix) {
+    public void registerChestRecipe(RecipeExporter exporter, Map<Identifier, Block> chests, String modId, String plankSuffix) {
         for (Map.Entry<Identifier, Block> entry : chests.entrySet()) {
             Identifier chestId = entry.getKey();
             Block chest = entry.getValue();
